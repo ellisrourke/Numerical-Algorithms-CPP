a9fda0b98688e2112d15af0924032d0a8686a22c
ellisrourke <ellisrourke@gmail.com>
2021-04-19 20:40:16 +1000

    added error calc to q1

--- q1-central-difference-Done-maybe/centralDifference.cpp
+++ q1-central-difference-Done-maybe/centralDifference.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <limits>
+#include <iomanip>
 using namespace std;
 #define N 5
 
@@ -12,6 +14,13 @@ double derivativeFunction(double x){
   return ((-0.4*pow(x,3)) - (0.45*pow(x,2)) - (x) -0.15);
 }
 
+double secondDerivative(double x){
+  return( (-1.2*pow(x,2)) - 0.9*x - 1 );
+}
+
+double thirdDerivative(double x){
+  return( -2.4*x - 0.9);
+}
 double centralDifference(double (*f)(double), double x, double h){
   return (f(x + h) - f(x - h)) / (2 * h);
 }
@@ -19,18 +28,21 @@ double centralDifference(double (*f)(double), double x, double h){
 
 int main(){
   double x = 0.5;
-  double h = 1.0;
-  double hValues[] = {5,4,3,2,1,0.5,0.25,0.1,0.01,0.001};
+  double hValues[] = {10,5,4,3,2,1.5,1,0.5,0.25,0.1};
 
-
-
+  double calculatedValue;
+  double roundOffError;
+  double truncationError;
   cout << "at x = " << x << endl;
   cout << "function = " << function(x) << endl;
   cout << "derivative = " << derivativeFunction(x) << endl << endl;
-
   for(int i=0;i<10;i++){
-    cout << "Central Difference approximation (h = " << hValues[i] << ") = "<< centralDifference(function,  x,  hValues[i]) << endl;
-    cout << "Error = " << centralDifference(function,  x,  hValues[i]) - ((centralDifference(function,  x+1,  hValues[i]) - centralDifference(function,  x-1,  hValues[i])) / (2*h)) << endl;
-
+    calculatedValue = centralDifference(function,  x,  hValues[i]);
+    roundOffError = calculatedValue - ((centralDifference(function,  x+1,  hValues[i]) - centralDifference(function,  x-1,  hValues[i])) / (2* hValues[i]));
+    truncationError = (thirdDerivative(numeric_limits<double>::epsilon())/6) * (hValues[i] * hValues[i]);
+    cout << "Central Difference approximation (h = " << hValues[i] << ") = " << calculatedValue << endl;
+    cout << "Round off error = " << roundOffError << endl;
+    cout << "Truncation error = " << truncationError << endl;
+    cout << "True value = " << (calculatedValue + roundOffError - truncationError) << endl << endl;
   }
 }
